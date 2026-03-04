a0e9c4dfbeaf47ec6008a58e7f2c0c9f31a56b9c
Danila Yanvarev <danilayanvarev@MacBook-Air-Danila.local>
2026-03-04 10:19:42 +0300

    fix views

--- backend/Dashboards/Infrastructure/Persistence/Migrations/20260301183251_AchivementView.cs
+++ backend/Dashboards/Infrastructure/Persistence/Migrations/20260301183251_AchivementView.cs
@@ -11,26 +11,26 @@ namespace Infrastructure.Persistence.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-            CREATE VIEW dictionary.""Достижения"" AS
+            CREATE VIEW university.""Достижения"" AS
             SELECT  s.""Fio"" as ""ФИО студента"",
                     c.""Name"" as ""Категория"",
                     a.""BeginDate"" as ""Дата присвоения""
             FROM university.""Achivment"" a
-            LEFT JOIN dictionary.""Student"" s ON s.""Id"" = a.""StudentId""
+            LEFT JOIN university.""Student"" s ON s.""Id"" = a.""StudentId""
             LEFT JOIN dictionary.""AchivmentCategory"" c ON c.""Id"" = a.""CategoryId""
         ");
 
             migrationBuilder.Sql(@"
-                GRANT USAGE ON SCHEMA dictionary TO metabase;
-                GRANT SELECT ON dictionary.""Достижения"" TO metabase;
+                GRANT USAGE ON SCHEMA university TO metabase;
+                GRANT SELECT ON university.""Достижения"" TO metabase;
             ");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("REVOKE SELECT ON dictionary.\"Достижения\" FROM metabase;");
-            migrationBuilder.Sql("DROP VIEW IF EXISTS dictionary.\"Достижения\";");
+            migrationBuilder.Sql("REVOKE SELECT ON university.\"Достижения\" FROM metabase;");
+            migrationBuilder.Sql("DROP VIEW IF EXISTS university.\"Достижения\";");
         }
     }
 }
--- backend/Dashboards/Infrastructure/Persistence/Migrations/20260301183316_OrderView.cs
+++ backend/Dashboards/Infrastructure/Persistence/Migrations/20260301183316_OrderView.cs
@@ -11,26 +11,26 @@ namespace Infrastructure.Persistence.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-            CREATE VIEW dictionary.""Приказ"" AS
+            CREATE VIEW university.""Приказ"" AS
             SELECT  s.""Fio"" as ""ФИО студента"",
                     c.""Name"" as ""Подкатегория"",
                     o.""Date"" as ""Дата""
             FROM university.""Order"" o
-            LEFT JOIN dictionary.""Student"" s ON s.""Id"" = o.""StudentId""
+            LEFT JOIN university.""Student"" s ON s.""Id"" = o.""StudentId""
             LEFT JOIN dictionary.""OrderCategory"" c ON c.""Id"" = o.""CategoryId""
         ");
 
             migrationBuilder.Sql(@"
-                GRANT USAGE ON SCHEMA dictionary TO metabase;
-                GRANT SELECT ON dictionary.""Приказ"" TO metabase;
+                GRANT USAGE ON SCHEMA university TO metabase;
+                GRANT SELECT ON university.""Приказ"" TO metabase;
             ");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("REVOKE SELECT ON dictionary.\"Приказ\" FROM metabase;");
-            migrationBuilder.Sql("DROP VIEW IF EXISTS dictionary.\"Приказ\";");
+            migrationBuilder.Sql("REVOKE SELECT ON university.\"Приказ\" FROM metabase;");
+            migrationBuilder.Sql("DROP VIEW IF EXISTS university.\"Приказ\";");
         }
     }
 }
